4187abbb70a0ebe4f4e880e9a25e29d8afcb2b11
Christian Gollee <christian.gollee@iwu.fraunhofer.de>
2024-12-04 21:18:09 +0100

    - docs version 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2024, Fraunhofer IWU"
 author = "Christian Gollee"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.0"
+release = "1.0.0"
 
 
 # -- General configuration ---------------------------------------------------
